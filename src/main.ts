6a8f39a1642257a6cb686cafa589b61fd70e01af
Bonaventura Galang <bonaventuragal@gmail.com>
2024-05-22 05:50:57 +0000

    feat: add port env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
+
+  Logger.log(`Listening on ${await app.getUrl()}`)
 }
 bootstrap();
