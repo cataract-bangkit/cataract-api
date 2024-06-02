d10798b0c8167369613b62280b74d805794fd1ec
Aidah Novallia putri <aidahptr15@gmail.com>
2024-06-02 13:17:45 +0000

    feat: add predict endpoints incomplete

--- src/predict/predict.controller.ts
+++ src/predict/predict.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { PredictService } from './predict.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { IsPublic, fail, success } from 'src/utils';
 
 @Controller('predict')
 export class PredictController {
-  constructor(private readonly predictService: PredictService) {}
+  constructor(private readonly predictService: PredictService) { }
 
   @Post()
+  @IsPublic()
   @UseInterceptors(FileInterceptor('image'))
-  async predict(@UploadedFile() img: Express.Multer.File) {}
+  async predict(@UploadedFile() img: Express.Multer.File) {
+    if (!img) {
+      throw new BadRequestException(fail('image is required.'))
+    }
+
+    const result = await this.predictService.predict(img)
+    return success("Successfully predicted", { result })
+  }
 }
--- src/predict/predict.service.ts
+++ src/predict/predict.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, PayloadTooLargeException } from "@nestjs/common";
+import { ResponseErrorCode, fail } from "src/utils";
 
 @Injectable()
-export class PredictService {}
+export class PredictService {
+	async predict(img: Express.Multer.File) {
+		if (img.size > 5000000) {
+			throw new PayloadTooLargeException(fail("Image too large. Maximum image size is 5mb.", ResponseErrorCode.ERR_5))
+		}
+
+		const filename = img.originalname.toLowerCase()
+
+		if (!filename.endsWith("png") && !filename.endsWith("jpg") && !filename.endsWith("jpeg")) {
+			throw new BadRequestException(fail("File has invalid extension. Allowed extensions are png/jpg/jpeg.", ResponseErrorCode.ERR_6))
+		}
+
+		try {
+			// TODO
+		} catch (e) {
+			throw new BadRequestException(fail("Prediction failed.", ResponseErrorCode.ERR_7))
+		}
+	}
+}
